d9f3f5832baabf8ed1a6d11e543f4587906b486d
czx <1924086038@qq.com>
2024-11-09 21:16:24 +0800

    fix

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
+  site_name: '玩转Code',
+  title: '玩转Code',
   description: 'Boilerplate built with Astro using React and Tailwind CSS',
   author: 'Emma',
   locale_region: 'en-us',
