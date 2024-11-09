f7c3a12dc2f6c091ccf84887d925fd7a0a9e79fa
czx <1924086038@qq.com>
2024-11-09 20:49:07 +0800

    init

---
+++ src/partials/Footer.tsx
@@ -0,0 +1,12 @@
+import { FooterCopyright, Section } from 'astro-boilerplate-components';
+
+import { AppConfig } from '@/utils/AppConfig';
+
+const Footer = () => (
+  <Section>
+    {/* <FooterCopyright site_name={AppConfig.site_name} /> */}
+    © Copyright 2024 玩转Code 程序员陈师兄
+  </Section>
+);
+
+export { Footer };
---
+++ src/partials/Navbar.tsx
@@ -0,0 +1,46 @@
+import {
+  Logo,
+  NavbarTwoColumns,
+  NavMenu,
+  NavMenuItem,
+  Section,
+} from 'astro-boilerplate-components';
+
+const Navbar = () => (
+  <Section>
+    <NavbarTwoColumns>
+      <a href="/">
+        <Logo
+          icon={
+            <svg
+              className="mr-1 h-10 w-10 stroke-cyan-600"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M0 0h24v24H0z" stroke="none"></path>
+              <rect x="3" y="12" width="6" height="8" rx="1"></rect>
+              <rect x="9" y="8" width="6" height="12" rx="1"></rect>
+              <rect x="15" y="4" width="6" height="16" rx="1"></rect>
+              <path d="M4 20h14"></path>
+            </svg>
+          }
+          name="玩转Code"
+        />
+      </a>
+      <NavMenu>
+        <NavMenuItem href="/">首页</NavMenuItem>
+        <NavMenuItem href="#cxycsx">程序员陈师兄</NavMenuItem>
+        {/* <NavMenuItem href="/">JS逆向实战</NavMenuItem> */}
+        <NavMenuItem href="https://space.bilibili.com/409871996" target="_blank">B站</NavMenuItem>
+        <NavMenuItem href="#problem">常见问题</NavMenuItem>
+        <NavMenuItem href="http://upload.cxycsx.vip/a1cd99e6b709dc95f44abd520ad5d75.jpg" target="_blank">联系我</NavMenuItem>
+      </NavMenu>
+    </NavbarTwoColumns>
+  </Section>
+);
+
+export { Navbar };
